c046d64bed91fb8c95cfddf36ba6bfb5f0a5281f
Sam Hall <mann.samantha11@gmail.com>
2021-06-07 21:04:21 -0400

    NavBar

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from "./NavBar";
+
+function App() {
+
+  return(
+    <NavBar />
+
+  );
+
+}
+
+// TEST COMMIT
+export default App;
---
+++ src/NavBar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function NavBar(){
+return(
+<h1>Sam Hall - Stand Up Comic</h1>
+)
+}
+
+export default NavBar;
