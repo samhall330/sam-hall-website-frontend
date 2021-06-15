9df8da440ab7c27f1066b4b0ff9e13f4cdf8b073
Sam Hall <mann.samantha11@gmail.com>
2021-06-14 23:26:29 -0400

    about me page content and first styling

--- src/About.js
+++ src/About.js
@@ -2,8 +2,10 @@ import React from "react";
 
 function About(){
 return(
-<h1 class="title-header">About me!</h1>
-
+    <>
+<h1 class="title-header">About me...</h1>
+<h3 class="about-me-text">Hey! I'm a New York transplant and Oklahoma native with a knack for observational humor about my life, my upbringing (read, " light beatings") and the proceeding trauma, weird sexual encounters and general body horror that came with the package.</h3>
+</>
 )
 }
 
