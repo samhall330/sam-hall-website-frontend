dea5f48b41ad15764c5a6fb2fa88f3a89a6d71e0
Sam Hall <mann.samantha11@gmail.com>
2021-06-14 23:31:40 -0400

    upcoming shows tab styling and bullets

--- src/UpcomingShows.js
+++ src/UpcomingShows.js
@@ -2,7 +2,13 @@ import React from "react";
 
 function UpcomingShows(){
 return(
+    <>
 <h1 class="title-header">Show Dates!</h1>
+<ul class= "tour-date-list">
+    <li>Industry Room - Broadway Comedy Club - July 10th</li>
+    <li>Broadway Comedy Club - July 29th</li>
+</ul>
+</>
 
 )
 }
