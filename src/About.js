0e0dab2e966066dd8f3db15b81379968f572a929
Sam Hall <mann.samantha11@gmail.com>
2021-06-21 21:47:38 -0400

    attempt to fix png issue

--- src/About.js
+++ src/About.js
@@ -2,9 +2,9 @@ import React from "react";
 
 function About(){
 return(
-    <>
+<>
 <h1 class="title-header">About me...</h1>
-<img src="samhallcomedian.png" alt="Sam Hall Performing at the Brooklyn Museum" width="500" height="600"/>
+<img src="samhallcomedian.jpg" alt="Sam Hall Performing at the Brooklyn Museum" width="500" height="600"/>
 <h3 class="about-me-text">Hey! I'm a New York transplant and Oklahoma native with a knack for observational humor about my life, my upbringing (read, " light beatings") and the proceeding trauma, weird sexual encounters and general body horror that came with the package.</h3>
 </>
 )
