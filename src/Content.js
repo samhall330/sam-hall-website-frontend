9648ab9f38b7844dfaf299380e67444f983f0018
Sam Hall <mann.samantha11@gmail.com>
2021-06-28 22:14:29 -0400

    Content page updates, links, functionality

--- src/Content.js
+++ src/Content.js
@@ -5,8 +5,13 @@ return(
     <>
 <h1 class="title-header">My Content!</h1>
 <ul class= "tour-date-list">
-    <li><a href="https://www.instagram.com/tv/B-L8ACeA7UT/"> <p>Mascara is Over Sexualized</p> </a></li>
-    <li><a href="https://www.instagram.com/tv/CDj43MyAUds/"> <p>Great Interracial Baking Show - Episode 1</p> </a></li>
+    <li><a href="https://www.instagram.com/tv/B-L8ACeA7UT/"> <p>Mascara is Over Sexualized (Writer, Editor)</p> </a></li>
+    <li><a href="https://www.instagram.com/tv/CDj43MyAUds/"> <p>Great Interracial Baking Show - Episode 1 "Introductions" (Actor, Producer, Editor)</p> </a></li>
+    <li><a href="https://www.instagram.com/tv/CEsK902Dy8A/"> <p>Great Interracial Baking Show - Episode 2 "Buttered Toast" (Producer, Editor)</p> </a></li>
+    <li><a href="https://www.instagram.com/tv/CFIhh4CgbBU/"> <p>Great Interracial Baking Show - Episode 3 "Caucasian Cornbread" (Actor, Producer, Editor)</p> </a></li>
+    <li><a href="https://www.instagram.com/tv/CFs0oSOAC_D/"> <p>"Scary Toilet"(Producer, Director, Editor)</p> </a></li>
+    <li><a href="https://www.youtube.com/watch?v=p6YyimoY47Y&ab_channel=HaleynaKociuk-Garza"> <p>"Horror Derves - Episode 1 - What Doesn't Kill You Makes You Stranger" (Actor- "Mom")</p> </a></li>
+
 </ul>
 </>
 )
