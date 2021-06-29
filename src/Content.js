9f69aebeab76eecd79736afa567c4d81d00c5a43
Sam Hall <mann.samantha11@gmail.com>
2021-06-28 22:03:27 -0400

    Content Page Links

--- src/Content.js
+++ src/Content.js
@@ -2,8 +2,13 @@ import React from "react";
 
 function Content(){
 return(
+    <>
 <h1 class="title-header">My Content!</h1>
-
+<ul class= "tour-date-list">
+    <li><a href="https://www.instagram.com/tv/B-L8ACeA7UT/"> <p>Mascara is Over Sexualized</p> </a></li>
+    <li><a href="https://www.instagram.com/tv/CDj43MyAUds/"> <p>Great Interracial Baking Show - Episode 1</p> </a></li>
+</ul>
+</>
 )
 }
 
