0243979a3fc99added58dbaf5621ff081563d798
Sam Hall <mann.samantha11@gmail.com>
2021-07-12 23:26:34 -0400

    more content thumbnail links

--- src/Content.js
+++ src/Content.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactPlayer from 'react-player'
 import mascara from './mascara-screenshot.jpg'
+import gibs1 from './gibs-ep1.jpg'
+import gibs2 from './gibs-ep2.jpg'
 
 function Content(){
 return(
@@ -8,10 +10,12 @@ return(
 <h1 class="title-header">Check Out My Content</h1>
 <ul class= "tour-date-list">
     <h1>Hot Bits!</h1>
-    <img src={mascara} alt="an audio recording of a stand up" width="200" height="100"></img>
+    <img src={mascara} alt="audio recording of a stand up bit by Sam Hall" width="400" height="200"></img>
     <li><a href="https://www.instagram.com/tv/B-L8ACeA7UT/"> <p>Mascara is Over Sexualized (Writer, Editor)</p> </a></li>
     <h1>Series</h1>
+    <img src={gibs1} alt="the great interracial baking show episode 1" width="400" height="200"></img>
     <li><a href="https://www.instagram.com/tv/CDj43MyAUds/"> <p>Great Interracial Baking Show - Episode 1 "Introductions" (Actor, Producer, Editor)</p> </a></li>
+    <img src={gibs2} alt="the great interracial baking show episode 2" width="400" height="200"></img>
     <li><a href="https://www.instagram.com/tv/CEsK902Dy8A/"> <p>Great Interracial Baking Show - Episode 2 "Buttered Toast" (Producer, Editor)</p> </a></li>
     <li><a href="https://www.instagram.com/tv/CFIhh4CgbBU/"> <p>Great Interracial Baking Show - Episode 3 "Caucasian Cornbread" (Actor, Producer, Editor)</p> </a></li>
     <h1>Short Shorts</h1>
