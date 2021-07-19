df7505e0e0aefaf43f3f5d27a34da7413576279c
Sam Hall <mann.samantha11@gmail.com>
2021-07-19 05:50:53 -0400

    about me photo border animation and link

--- src/About.js
+++ src/About.js
@@ -6,7 +6,9 @@ return(
     <>
 <h1 class="title-header">About me...</h1>
 <h3 class="about-me-text">Hey! I'm a New York transplant and Oklahoma native with a knack for observational humor about my life, my upbringing (read, " light beatings") and the proceeding trauma, weird sexual encounters and general body horror that came with the package.</h3>
+<a href="https://www.instagram.com/samhallllllllll/">
 <img class="about-img" src={image} alt="Sam Hall Performing at the Brooklyn Museum" width="500" height="600"></img>
+</a>
 </>
 )
 }
