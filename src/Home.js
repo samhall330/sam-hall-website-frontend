7cd83e045b61340c3fce6c4351545a765e9bbf79
Sam Hall <mann.samantha11@gmail.com>
2021-07-19 06:16:40 -0400

    Homepage tape and styling

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
+import YouTube from 'react-youtube';
+import ReactPlayer from "react-player";
 
 function Home(){
 return(
     <>
 <h1 class="title-header">Sam Hall - Stand Up Comic</h1>
 
-<h3 class="broadway-tape"> This is where my Broadway Comedy Club tape will go!</h3>
+<h3 class="broadway-tape-header">Broadway Comedy Club - July 10th, 2021</h3>
+      <ReactPlayer class="broadway-tape"url="https://youtu.be/Jo-AXTqQAME"/>
 </>
 )
 }
