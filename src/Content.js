a6b926700ad3022a4d16d2a691998398130db18d
Sam Hall <mann.samantha11@gmail.com>
2021-07-19 05:27:19 -0400

    fixing interactive image links for content page

--- src/Content.js
+++ src/Content.js
@@ -13,26 +13,37 @@ import Link from "react-router-dom"
 function Content(){
 return(
     <>
-<h1 class="title-header">Check Out My Content</h1>
+<h1 class="title-header">Check Out My Content!</h1>
 <ul class= "tour-date-list">
     <h1>Hot Bits!</h1>
     <a href="https://www.instagram.com/tv/B-L8ACeA7UT/">
     <img src={mascara} alt="audio recording of a stand up bit by Sam Hall" width="400" height="200"></img>
     </a>
+    <h5>Mascara is Over Sexualized (Writer, Editor)</h5>
+    <br></br>
     <h1>Series</h1>
+    <a href="https://www.instagram.com/tv/CDj43MyAUds/">
     <img src={gibs1} alt="the great interracial baking show episode 1" width="400" height="200"></img>
-    <li><a href="https://www.instagram.com/tv/CDj43MyAUds/"> <p>Great Interracial Baking Show - Episode 1 "Introductions" (Actor, Producer, Editor)</p> </a></li>
+    </a>
+    <h5>Great Interracial Baking Show - Episode 1 "Introductions" (Actor, Producer, Editor)</h5>
+    <br></br>
+    <a href="https://www.instagram.com/tv/CEsK902Dy8A/">
     <img src={gibs2} alt="the great interracial baking show episode 2" width="400" height="200"></img>
-    <li><a href="https://www.instagram.com/tv/CEsK902Dy8A/"> <p>Great Interracial Baking Show - Episode 2 "Buttered Toast" (Producer, Editor)</p> </a></li>
+    </a>
+    <h5>Great Interracial Baking Show - Episode 2 "Buttered Toast" (Producer, Editor)</h5>
+    <br></br>
+    <a href="https://www.instagram.com/tv/CFIhh4CgbBU/">
     <img src={gibs3} alt="the great interracial baking show episode 3" width="400" height="200"></img>
-    <li><a href="https://www.instagram.com/tv/CFIhh4CgbBU/"> <p>Great Interracial Baking Show - Episode 3 "Caucasian Cornbread" (Actor, Producer, Editor)</p> </a></li>
+    </a>
+    <h5>Great Interracial Baking Show - Episode 3 "Caucasian Cornbread" (Actor, Producer, Editor)</h5>
+    <br></br>
     <h1>Short Shorts</h1>
     <img src={scary} alt="a comedy short film called Scary Toilet" width="400" height="200"></img>
     <li><a href="https://www.instagram.com/tv/CFs0oSOAC_D/"> <p>"Scary Toilet"(Producer, Director, Editor)</p> </a></li>
     <img src={mumfights} alt="a comedy sketch called Mum Fights" width="400" height="200"></img>
     <li><a href="https://www.instagram.com/p/CMaGliqA_LY/"> <p>"Mum Fights"(Producer, Actor, Director, Editor)</p> </a></li>
+    <br></br>
     <h1>Acting!</h1>
-    {/* <ReactPlayer url="https://www.instagram.com/tv/B-L8ACeA7UT/" /> */}
     <img src={stranger} alt="Horror Derves Episode 1" width="400" height="200"></img>
     <li><a href="https://www.youtube.com/watch?v=p6YyimoY47Y&ab_channel=HaleynaKociuk-Garza"> <p>"Horror Derves - Episode 1 - What Doesn't Kill You Makes You Stranger" (Actor- "Mom")</p> </a></li>
 
