49c0ef259fd559e53da8a49dffc492738af68a46
Sam Hall <mann.samantha11@gmail.com>
2021-07-26 20:59:31 -0400

    social links email container rework

--- src/About.js
+++ src/About.js
@@ -1,5 +1,5 @@
 import React from "react";
-import image from "./samhallcomedian.jpg";
+import image from "./industry-room.png";
 
 function About(){
 return(
@@ -7,7 +7,7 @@ return(
 <h1 class="title-header">About me...</h1>
 <h3 class="about-me-text">Hey! I'm a New York transplant and Oklahoma native with a knack for observational humor about my life, my upbringing (read, " light beatings") and the proceeding trauma, weird sexual encounters and general body horror that came with the package.</h3>
 <a href="https://www.instagram.com/samhallllllllll/">
-<img class="about-img" src={image} alt="Sam Hall Performing at the Brooklyn Museum" width="500" height="600"></img>
+<img class="about-img" src={image} alt="Sam Hall Performing at the Brooklyn Museum" width="450" height="600"></img>
 </a>
 </>
 )
--- src/Contact.js
+++ src/Contact.js
@@ -1,9 +1,19 @@
 import React from "react";
+import facebook from "./facebook.png"
+import instagram from "./instagram.png"
+import twitter from "./twitter.png"
+import youtube from "./youtube.png"
 
 function Contact(){
 return(
     <>
 <h1 class="title-header">Contact me!</h1>
+    <div class="social-links">
+        <img src={instagram}></img>
+        <img src={twitter}></img>
+        <img src={facebook}></img>
+        <img src={youtube}></img>
+    </div>
     <form class="email-box" action="mailto:samhallcomedy@gmail.com" method="post" enctype="text/plain">Contact via email (if you're old)<br></br>     
         <input class ="email-button" type="submit" value="Drop me a line!"/>
     </form>
