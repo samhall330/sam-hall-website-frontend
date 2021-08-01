150fc7cb8f07be6fd8774d14ebf720ab4d129a65
Sam Hall <mann.samantha11@gmail.com>
2021-08-01 16:33:06 -0400

    show date calendar

--- src/UpcomingShows.js
+++ src/UpcomingShows.js
@@ -2,15 +2,74 @@ import React from "react";
 
 function UpcomingShows(){
 return(
-    <>
+<>
 <h1 class="title-header">Show Dates!</h1>
+<div class="month">      
+  <ul>
+   
+    <li>
+      August<br></br>
+      <span>2021</span>
+    </li>
+  </ul>
+</div>
+
+<ul class="weekdays">
+  <li>Mo</li>
+  <li>Tu</li>
+  <li>We</li>
+  <li>Th</li>
+  <li>Fr</li>
+  <li>Sa</li>
+  <li>Su</li>
+</ul>
+
+<ul class="days">  
+  <li>26</li>
+  <li>27</li>
+  <li>28</li>
+  <li>29</li>
+  <li>30</li>
+  <li>31</li>
+  <li>1</li>
+  <li>8</li>
+  <li>9</li>
+  <li><span class="active">4</span></li>
+  <li>5</li>
+  <li>6</li>
+  <li>7</li>
+  <li>8</li>
+  <li>9</li>
+  <li>10</li>
+  <li>11</li>
+  <li>12</li>
+  <li>13</li>
+  <li>14</li>
+  <li>15</li>
+  <li>16</li>
+  <li>17</li>
+  <li>18</li>
+  <li>19</li>
+  <li>20</li>
+  <li>21</li>
+  <li>22</li>
+  <li>23</li>
+  <li>24</li>
+  <li>25</li>
+  <li>26</li>
+  <li>27</li>
+  <li>28</li>
+  <li>29</li>
+  <li>30</li>
+  <li>31</li>
+</ul>
+
 <div class= "tour-date-list">
-    <a href="https://www.broadwaycomedyclub.com/events/the-industryroom-2021-07-10180000"> <p>July 10th - Industry Room - Broadway Comedy Club</p> </a>
-    <a href="https://www.broadwaycomedyclub.com/"> <p>July 29th - Broadway Comedy Club</p> </a>
     <a href="https://greenwichvillagecomedyclub.com/calendar/2021-08?day=04"> <p>August 4th - Greenwich Village Comedy Club</p></a>
 </div>
-</>
 
+
+</>
 )
 }
 
