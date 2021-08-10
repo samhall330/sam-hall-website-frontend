a6619c99fa8c15da9e979bc2b79bec282f0c92f8
Sam Hall <mann.samantha11@gmail.com>
2021-08-09 21:52:04 -0400

    more header img styling

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
 import YouTube from 'react-youtube';
 import ReactPlayer from "react-player";
+import samhall from "./sam-hall.jpg"
 
 function Home(){
 return(
     <>
-<h1 class="title-header">Sam Hall - Stand Up Comic</h1>
+<h1 class="title-header">
+<img className="samhall-header" src={samhall} alt="About Me" width="400" height="300"></img>
+</h1>
 
 <h3 class="broadway-tape-header">Broadway Comedy Club - July 10th, 2021</h3>
       <ReactPlayer class="broadway-tape"url="https://youtu.be/Jo-AXTqQAME"/>
