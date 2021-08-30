eb4106668bebdec29b2f20616cdac4f7302fabf6
Sam Hall <mann.samantha11@gmail.com>
2021-08-30 12:02:56 -0400

    Upcoming Shows styling fixes

--- src/UpcomingShows.js
+++ src/UpcomingShows.js
@@ -22,20 +22,20 @@ return(
       <li>Sun</li>
     </ul>
     <ul className="days">  
-      <li>26</li>
-      <li>27</li>
-      <li>28</li>
-      <li>29</li>
       <li>30</li>
       <li>31</li>
       <li>1</li>
-      <li>8</li>
-      <li>9</li>
-      <li><span className="active">4</span></li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
       <li>5</li>
       <li>6</li>
       <li>7</li>
-      <li>8</li>
+      <li><span className="active">8</span></li>
+      <li>9</li>
+      <li>10</li>
+      <li>11</li>
+      <li>12</li>
       <li>9</li>
       <li>10</li>
       <li>11</li>
