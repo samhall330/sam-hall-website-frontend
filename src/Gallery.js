d85629de92f2a810ebf04d9c3f7fe25bce45e560
Sam Hall <mann.samantha11@gmail.com>
2021-10-11 23:36:18 -0400

    gallery content description styling

--- src/Gallery.js
+++ src/Gallery.js
@@ -3,7 +3,11 @@ import samYAS1 from "./samYAS1.jpg"
 
 function Gallery(){
     return(
-<img className="content-links" src={samYAS1} alt="audio recording of a stand up bit by Sam Hall" width="500" height="300"></img>
+        <div className="content-list">
+            <img className="gallery-image" src={samYAS1} alt="audio recording of a stand up bit by Sam Hall" width="500" height="350"></img>
+            <br></br>
+            <h5 className="content-caption">YAAAS! Fest 2021 - Queer Comedy Festival @ Broadway Comedy Club, NYC </h5>
+        </div>
     )}
 
 export default Gallery;
