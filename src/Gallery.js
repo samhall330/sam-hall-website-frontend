35b91b09d933fa0d6624088f34482ddf70f34ca0
Sam Hall <mann.samantha11@gmail.com>
2021-10-11 23:45:46 -0400

    gallery content formatted with interactive links

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,12 +1,21 @@
 import React from "react";
 import samYAS1 from "./samYAS1.jpg"
+import samYAS2 from "./samYAS2.jpg"
 
 function Gallery(){
     return(
         <div className="content-list">
-            <img className="gallery-image" src={samYAS1} alt="audio recording of a stand up bit by Sam Hall" width="500" height="350"></img>
+            <a href ="https://www.queercomedyfestival.com/">
+            <img className="gallery-image" src={samYAS1} alt="sam hall at yaaas fest 2021" width="700" height="500"></img>
+            </a>
             <br></br>
-            <h5 className="content-caption">YAAAS! Fest 2021 - Queer Comedy Festival @ Broadway Comedy Club, NYC </h5>
+            <h5 className="content-caption">YAAAS! Fest 2021 - Queer Comedy Festival @ Broadway Comedy Club, NYC - September 30, 2021</h5>
+            <br></br>
+            <a href ="https://www.queercomedyfestival.com/">
+            <img className="gallery-image" src={samYAS2} alt="sam hall at YAAAS fest 2021" width="700" height="500"></img>
+            </a>
+            <br></br>
+            <h5 className="content-caption">YAAAS! Fest 2021 - Queer Comedy Festival @ Broadway Comedy Club, NYC - September 30, 2021</h5>
         </div>
     )}
 
