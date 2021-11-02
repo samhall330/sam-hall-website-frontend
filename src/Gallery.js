2c82af696a3882aadaa373010ee7001703854cbb
Sam Hall <mann.samantha11@gmail.com>
2021-11-01 22:20:20 -0400

    gallery updates

--- src/Gallery.js
+++ src/Gallery.js
@@ -2,6 +2,7 @@ import React from "react";
 import samYAS1 from "./samYAS1.jpg"
 import samYAS2 from "./samYAS2.jpg"
 import barebackShow from "./bareback-show-pic.jpeg"
+import barebackShow2 from "./bareback-show-2.png"
 
 function Gallery(){
     return(
@@ -20,6 +21,12 @@ function Gallery(){
             <br></br>
             <a href ="https://www.eventbrite.com/e/the-bareback-show-a-stand-up-comedy-extravaganza-lukes-birthday-tickets-189908430227">
             <img className="gallery-image" src={barebackShow} alt="sam hall at bareback show october 2021" width="500" height="500"></img>
+            </a>
+            <br></br>
+            <h5 className="content-caption">Bareback Show - Talon Bar, Bushwick - October 22, 2021</h5>
+            <br></br>
+            <a href ="https://www.eventbrite.com/e/the-bareback-show-a-stand-up-comedy-extravaganza-lukes-birthday-tickets-189908430227">
+            <img className="gallery-image" src={barebackShow2} alt="sam hall at bareback show october 2021" width="700" height="500"></img>
             </a>
             <br></br>
             <h5 className="content-caption">Bareback Show - Talon Bar, Bushwick - October 22, 2021</h5>
