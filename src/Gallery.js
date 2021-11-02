d4873322ca85d52960919e00c4d87238e0136ce3
Sam Hall <mann.samantha11@gmail.com>
2021-11-01 22:29:29 -0400

    more gallery updates

--- src/Gallery.js
+++ src/Gallery.js
@@ -3,6 +3,7 @@ import samYAS1 from "./samYAS1.jpg"
 import samYAS2 from "./samYAS2.jpg"
 import barebackShow from "./bareback-show-pic.jpeg"
 import barebackShow2 from "./bareback-show-2.png"
+import barebackShow3 from "./bareback-show-3.png"
 
 function Gallery(){
     return(
@@ -26,10 +27,16 @@ function Gallery(){
             <h5 className="content-caption">Bareback Show - Talon Bar, Bushwick - October 22, 2021</h5>
             <br></br>
             <a href ="https://www.eventbrite.com/e/the-bareback-show-a-stand-up-comedy-extravaganza-lukes-birthday-tickets-189908430227">
-            <img className="gallery-image" src={barebackShow2} alt="sam hall at bareback show october 2021" width="700" height="500"></img>
+            <img className="gallery-image" src={barebackShow2} alt="sam hall and destiny dunkle at bareback show october 2021" width="700" height="500"></img>
             </a>
             <br></br>
             <h5 className="content-caption">Bareback Show w/ <a href= "https://www.instagram.com/denydurkle/">Destiny Dunkle</a> - Talon Bar, Bushwick - October 22, 2021</h5>
+            <br></br>
+            <a href ="https://www.eventbrite.com/e/the-bareback-show-a-stand-up-comedy-extravaganza-lukes-birthday-tickets-189908430227">
+            <img className="gallery-image" src={barebackShow3} alt="sam hall at bareback show october 2021" width="700" height="500"></img>
+            </a>
+            <br></br>
+            <h5 className="content-caption">Bareback Show - Talon Bar, Bushwick - October 22, 2021</h5>
         </div>
     )}
 
