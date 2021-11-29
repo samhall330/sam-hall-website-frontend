57593610f859562794bef61b5e65f8bdcd83233b
Sam Hall <mann.samantha11@gmail.com>
2021-11-29 17:52:01 -0500

    all interactive gallery links

--- src/Gallery.js
+++ src/Gallery.js
@@ -32,10 +32,14 @@ function Gallery(){
                 </a>
             </div>
             <div class="slides-item slide-4" id="slide-4">
-                <img className="gallery-image" src={samYAS1} alt="sam hall at yaaas fest 2021" width="700" height="500"></img>
+                <a href ="https://www.queercomedyfestival.com/">
+                    <img className="gallery-image" src={samYAS1} alt="sam hall at yaaas fest 2021" width="700" height="500"></img>
+                </a>
             </div>
             <div class="slides-item slide-5" id="slide-5">
-                <img className="gallery-image" src={samYAS2} alt="sam hall at YAAAS fest 2021" width="700" height="500"></img>    
+                <a href ="https://www.queercomedyfestival.com/">
+                    <img className="gallery-image" src={samYAS2} alt="sam hall at YAAAS fest 2021" width="700" height="500"></img>    
+                </a>
             </div>       
           </div>
           <div class="carousel__nav">
@@ -46,39 +50,6 @@ function Gallery(){
             <a class="slider-nav" href="#slide-5">5</a>
           </div>
         </section>
-
-
-        {/* <div className="content-list">
-            <a href ="https://www.queercomedyfestival.com/">
-            <img className="gallery-image" src={samYAS2} alt="sam hall at YAAAS fest 2021" width="700" height="500"></img>
-            </a>
-            <br></br>
-            <h5 className="content-caption">YAAAS! Fest 2021 - Queer Comedy Festival @ Broadway Comedy Club, NYC - September 30, 2021</h5>
-            <br></br>
-            <a href ="https://www.queercomedyfestival.com/">
-            <img className="gallery-image" src={samYAS1} alt="sam hall at yaaas fest 2021" width="700" height="500"></img>
-            </a>
-            <br></br>
-            <h5 className="content-caption">YAAAS! Fest 2021 - Queer Comedy Festival @ Broadway Comedy Club, NYC - September 30, 2021</h5>
-            <br></br>
-            <a href ="https://www.eventbrite.com/e/the-bareback-show-a-stand-up-comedy-extravaganza-lukes-birthday-tickets-189908430227">
-            <img className="gallery-image" src={barebackShow} alt="sam hall at bareback show october 2021" width="500" height="500"></img>
-            </a>
-            <br></br>
-            <h5 className="content-caption">Bareback Show - Talon Bar, Bushwick - October 22, 2021</h5>
-            <br></br>
-            <a href ="https://www.eventbrite.com/e/the-bareback-show-a-stand-up-comedy-extravaganza-lukes-birthday-tickets-189908430227">
-            <img className="gallery-image" src={barebackShow2} alt="sam hall and destiny dunkle at bareback show october 2021" width="700" height="500"></img>
-            </a>
-            <br></br>
-            <h5 className="content-caption">Bareback Show w/ <a href= "https://www.instagram.com/denydurkle/">Destiny Dunkle</a> - Talon Bar, Bushwick - October 22, 2021</h5>
-            <br></br>
-            <a href ="https://www.eventbrite.com/e/the-bareback-show-a-stand-up-comedy-extravaganza-lukes-birthday-tickets-189908430227">
-            <img className="gallery-image" src={barebackShow3} alt="sam hall at bareback show october 2021" width="700" height="500"></img>
-            </a>
-            <br></br>
-            <h5 className="content-caption">Bareback Show - Talon Bar, Bushwick - October 22, 2021</h5>
-        </div> */}
         </>
    
     )}
